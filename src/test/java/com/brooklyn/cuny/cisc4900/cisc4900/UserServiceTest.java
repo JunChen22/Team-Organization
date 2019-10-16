7b75ecd28b210fc01e7a5651204ad1c1a6e968b0
JunChen22 <ccjh522@gmail.com>
2019-10-16 17:37:22 -0400

    added travis build status and initial test case

--- src/test/java/com/brooklyn/cuny/cisc4900/cisc4900/UserServiceTest.java
+++ src/test/java/com/brooklyn/cuny/cisc4900/cisc4900/UserServiceTest.java
@@ -5,17 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc()
-//@TestPropertySource(locations = "classpath:/application-test.properties")
 public class UserServiceTest {
 
-    @Autowired
-    MockMvc mockMvc;
+    //need to change to does not need the  start the whole application to test a unit
+    //@Autowired
+    //MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
--- src/test/java/com/brooklyn/cuny/cisc4900/cisc4900/controller/MappingControllerTest.java
+++ src/test/java/com/brooklyn/cuny/cisc4900/cisc4900/controller/MappingControllerTest.java
@@ -4,14 +4,39 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc()
 public class MappingControllerTest {
+    /*
+    @Autowired
+    private MockMvc mockMvc;
 
+    @InjectMocks
+    private MappingController mappingController;
 
+    @Before
+    public void setUp() throws Exception{
+        mockMvc = MockMvcBuilders.standaloneSetup(mappingController).build();
+    }
+
+    */
+    @Test
+    public void testPublic() throws Exception{
+        /*
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/about")
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("at about us"));
+        */
+    }
 }
