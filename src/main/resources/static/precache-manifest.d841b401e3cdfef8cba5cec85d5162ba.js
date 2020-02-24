a347efdd42a87024076425df8c7e79363a1fc165
JunChen22 <ccjh522@gmail.com>
2020-02-23 21:10:25 -0500

    added view

---
+++ src/main/resources/static/precache-manifest.d841b401e3cdfef8cba5cec85d5162ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f23147c7fe0a06b7fa6b6d48f3c86e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647bf3e93270b6215ff5",
+    "url": "/static/css/2.5b4e953e.chunk.css"
+  },
+  {
+    "revision": "5e0c9dd336c84ec52787",
+    "url": "/static/css/main.5d47f2eb.chunk.css"
+  },
+  {
+    "revision": "647bf3e93270b6215ff5",
+    "url": "/static/js/2.8bfed890.chunk.js"
+  },
+  {
+    "revision": "5e0c9dd336c84ec52787",
+    "url": "/static/js/main.0788eca2.chunk.js"
+  },
+  {
+    "revision": "82e439bc74bf568c413d",
+    "url": "/static/js/runtime~main.1b56f11c.js"
+  }
+]);
